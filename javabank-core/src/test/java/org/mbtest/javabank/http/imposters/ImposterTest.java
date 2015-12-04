50dec27b6e28882054f5a0caf90a4d5e27470489
Tim Webster <tim.webster@gmail.com>
2015-12-04 11:56:09 +0000

    fix for issue #5 (on original) for missing response headers

--- javabank-core/src/test/java/org/mbtest/javabank/http/imposters/ImposterTest.java
+++ javabank-core/src/test/java/org/mbtest/javabank/http/imposters/ImposterTest.java
@@ -1,9 +1,12 @@
 package org.mbtest.javabank.http.imposters;
 
+import org.apache.http.HttpHeaders;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.mbtest.javabank.fluent.ImposterBuilder;
+import org.mbtest.javabank.http.core.Is;
 import org.mbtest.javabank.http.core.Stub;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -57,6 +60,43 @@ public class ImposterTest {
         expectedJson.put("protocol", expectedProtocol);
         JSONArray stubs = new JSONArray();
         stubs.add(new Stub());
+        expectedJson.put("stubs", stubs);
+
+        assertThat(imposter.toJSON()).isEqualTo(expectedJson);
+    }
+
+    @Test
+    public void shouldCreateAJsonObjectWithBuilder() {
+
+        String expectedBody = "Hello World!";
+        int expectedStatusCode = 201;
+        String expectedContentType = "plain/text";
+
+        Imposter imposter = ImposterBuilder.anImposter().onPort(expectedPort)
+                .stub()
+                  .response()
+                    .is()
+                      .body(expectedBody)
+                      .statusCode(expectedStatusCode)
+                      .header(com.google.common.net.HttpHeaders.CONTENT_TYPE, expectedContentType)
+                    .end()
+                  .end()
+                .end().build();
+
+        final Is response = new Is();
+        response.withHeader(HttpHeaders.CONTENT_TYPE, expectedContentType);
+        response.withBody(expectedBody);
+        response.withStatusCode(expectedStatusCode);
+
+        final Stub stub = new Stub();
+        stub.addResponse(response);
+
+        JSONArray stubs = new JSONArray();
+        stubs.add(stub);
+
+        JSONObject expectedJson = new JSONObject();
+        expectedJson.put("port", expectedPort);
+        expectedJson.put("protocol", expectedProtocol);
         expectedJson.put("stubs", stubs);
 
         assertThat(imposter.toJSON()).isEqualTo(expectedJson);
