6160a53fd532b499565ea5393d7677eca1b5e20c
josede2 <joseph.denman@vsp.com>
2018-10-31 15:49:08 -0700

    change 'data' back to private in Predicate class and make some class variables and methods protected to access in PredicateGenerator class

--- javabank-core/src/main/java/org/mbtest/javabank/http/predicates/Predicate.java
+++ javabank-core/src/main/java/org/mbtest/javabank/http/predicates/Predicate.java
@@ -10,20 +10,25 @@ import org.json.simple.JSONObject;
 public class Predicate extends HashMap {
 
 	private static final String XPATH = "xpath";
-	private static final String PATH = "path";
+	protected static final String PATH = "path";
 	private static final String METHOD = "method";
-	private static final String QUERY = "query";
+	protected static final String QUERY = "query";
 	private static final String HEADERS = "headers";
 	private static final String REQUEST_FROM = "requestFrom";
 	private static final String BODY = "body";
 
-	protected Map data;
+	private Map data;
 	private PredicateType type;
 
 	public Predicate(PredicateType type) {
 		this.type = type;
 		data = newHashMap();
 		this.put(type.getValue(), data);
+	}
+
+	protected Predicate addEntry(String key, boolean value) {
+		data.put(key, value);
+		return this;
 	}
 
 	private Predicate addEntry(String key, String value) {
--- javabank-core/src/main/java/org/mbtest/javabank/http/predicates/PredicateGenerator.java
+++ javabank-core/src/main/java/org/mbtest/javabank/http/predicates/PredicateGenerator.java
@@ -2,16 +2,8 @@ package org.mbtest.javabank.http.predicates;
 
 public class PredicateGenerator extends Predicate {
 
-	private static final String PATH = "path";
-	private static final String QUERY = "query";
-
 	public PredicateGenerator(PredicateType type) {
 		super(type);
-	}
-
-	private PredicateGenerator addEntry(String key, boolean value) {
-		data.put(key, value);
-		return this;
 	}
 
 	public PredicateGenerator withQueryParameters() {
